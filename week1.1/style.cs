32a51a97d3980530d45cfaee7f0650b79a7b47fc
ElisaRumSolberg <rumeysaince07@gmail.com>
2024-10-15 11:22:26 +0200

    2.commit

---
+++ week1.1/style.cs
@@ -0,0 +1,16 @@
+{
+    margin:0:
+}
+p{
+   /* margin:12px 0px 20px 0px;
+    margin-top:
+    margin-bottom:
+
+*/
+    margin-inline: 10px;
+
+    margin-block:12px
+}
+body{
+    background-color: bisque;
+}
